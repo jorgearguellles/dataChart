bebf9367c70b8435459ef2e7cf8dccd780a4268b
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-09 21:19:49 -0500

    update/ index.js logical done

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const width = +svg.attr("width");
 const height = +svg.attr("height");
 
 const render = data => {
-
   const xValue = d => d.utilizacion;
   const yValue = d => unixTimeToHumanTime(d.timestamp);
+  const margin = {top: 20, right:40, left:250, bottom:20};
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
 
   const xScale = d3.scaleLinear()
     .domain([0, d3.max(data, xValue)])
-    .range([0, width]);
+    .range([0, innerWidth]);
   
   const yScale = d3.scaleBand()
     .domain(data.map(yValue))
-    .range([0, height]);
-  
+    .range([0, innerHeight])
+    .padding(0.1);
     //console.log(yScale.domain());
 
-  svg.selectAll("rect").data(data)
+  const g = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  g.append("g").call(d3.axisLeft(yScale));
+  g.append("g").call(d3.axisBottom(xScale))
+  .attr("transform", `translate(0, ${innerHeight})`);
+
+  g.selectAll("rect").data(data)
     .enter().append("rect")
       .attr("y", d => yScale(yValue(d)))
       .attr("width", d => xScale(xValue(d)))
